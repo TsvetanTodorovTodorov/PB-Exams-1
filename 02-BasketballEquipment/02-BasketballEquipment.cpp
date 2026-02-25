679ce5c096c3f2e697db30b30bccf66cfe93840a
Tsvetan Todorov <111016955+TsvetanTodorovTodorov@users.noreply.github.com>
2026-02-25 08:27:31 +0200

    pb-exam1-basketbalEquipment

---
+++ 02-BasketballEquipment/02-BasketballEquipment.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int annualTax;
+    cin >> annualTax;
+
+    // sneaker;
+    double sneakers = annualTax - (0.4 * annualTax);
+    
+    //equipment;
+    double equipment = sneakers - (0.2 * sneakers);
+
+    // ball;
+    double ball = 0.25 * equipment;
+
+    // accessories;
+    double accessories = 0.2 * ball;
+
+    double expenses = annualTax + sneakers + equipment + ball + accessories;
+
+    cout.setf(ios::fixed);
+    cout.precision(2);
+
+    cout << expenses << endl;
+
+	return 0;
+}
