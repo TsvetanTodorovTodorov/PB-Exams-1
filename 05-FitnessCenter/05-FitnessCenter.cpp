6e7abea4d243b19c93ea8f84ad9b0cb5931dd96e
Tsvetan Todorov <111016955+TsvetanTodorovTodorov@users.noreply.github.com>
2026-02-28 13:34:02 +0200

    1 for loop with conditional if else if

---
+++ 05-FitnessCenter/05-FitnessCenter.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	// the goal is to calculate the number of
+	// ppl that goes to the fitness
+	int people; // number of ppl for a day
+	cin >> people;
+
+	// every other input is info what every person
+	// is doing
+
+	int back = 0;
+	int chest = 0;
+	int legs = 0;
+	int abs = 0;
+	int proteinShake = 0;
+	int proteinBar = 0;
+	double workOut = 0.0;
+	double protein = 0.0;
+
+		cin.ignore();
+	string goal;
+
+	for (int i = 1; i <= people; i++)
+	{
+		getline(cin, goal);
+
+		if (goal == "Back")
+		{
+			back++;
+			workOut++;
+		}
+		else if (goal == "Chest")
+		{
+			chest++;
+			workOut++;
+		}
+		else if (goal == "Legs")
+		{
+			legs++;
+			workOut++;
+		}
+		else if (goal == "Abs")
+		{
+			abs++;
+			workOut++;
+		}
+		else if (goal == "Protein shake")
+		{
+			proteinShake++;
+			protein++;
+		}
+		else if (goal == "Protein bar")
+		{
+			proteinBar++;
+			protein++;
+		}
+	}
+
+	double percentageWorkout = (workOut / people) * 100;
+	double percentageProtein = (protein / people) * 100;
+
+	cout << back << " - back" << endl;
+	cout << chest << " - chest" << endl;
+	cout << legs << " - legs" << endl;
+	cout << abs << " - abs" << endl;
+	cout << proteinShake << " - protein shake" << endl;
+	cout << proteinBar << " - protein bar" << endl;
+
+	cout.setf(ios::fixed);
+	cout.precision(2);
+
+	cout << percentageWorkout << "% - work out" << endl;
+	cout << percentageProtein << "% - protein" << endl;
+		
+
+	return 0;
+}
